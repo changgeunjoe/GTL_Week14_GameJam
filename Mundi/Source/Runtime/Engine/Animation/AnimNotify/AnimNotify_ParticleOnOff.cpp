5328da4482da8a737ce2dfb5e0fd24df8d94f81c
Soyeon Joo <sildredel@naver.com>
2025-12-07 12:34:56 +0900

    feat: Ribbon은 Animation으로 할 때만

---
+++ Mundi/Source/Runtime/Engine/Animation/AnimNotify/AnimNotify_ParticleOnOff.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "AnimNotify_ParticleOnOff.h"
+#include "Source/Runtime/Engine/Components/SkeletalMeshComponent.h"
+#include "Source/Runtime/Engine/Components/ParticleSystemComponent.h"
+#include "Source/Runtime/Core/Object/Actor.h"
+
+IMPLEMENT_CLASS(UAnimNotify_ParticleOnOff)
+
+UAnimNotify_ParticleOnOff::UAnimNotify_ParticleOnOff()
+{
+	bActivate = true;
+}
+
+void UAnimNotify_ParticleOnOff::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+
+	if (!MeshComp)
+	{
+		return;
+	}
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	for (UActorComponent* Component : Owner->GetOwnedComponents())
+	{
+		if (UParticleSystemComponent* ParticleComp = Cast<UParticleSystemComponent>(Component))
+		{
+			if (bActivate)
+			{
+				ParticleComp->ActivateSystem();
+			}
+			else
+			{
+				ParticleComp->DeactivateSystem();
+			}
+		}
+	}
+}
---
+++ Mundi/Source/Runtime/Engine/Animation/AnimNotify/AnimNotify_ParticleOnOff.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "AnimNotify.h"
+
+UCLASS(DisplayName = "파티클 On/Off")
+class UAnimNotify_ParticleOnOff : public UAnimNotify
+{
+public:
+	DECLARE_CLASS(UAnimNotify_ParticleOnOff, UAnimNotify);
+
+	UAnimNotify_ParticleOnOff();
+
+	void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+	UPROPERTY(EditAnywhere, Category = "Notify")
+	bool bActivate = true;
+};
